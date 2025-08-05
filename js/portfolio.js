f621f7bc637da3c519392cc9b1b43ecceba6e8dd
David-955 <dngo3819@gmail.com>
2025-08-05 14:17:58 +0200

    amélioration carrousel avec possibilité de tester les projets et modif du footer rgpd

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,150 @@
+// --- Carrousel dynamique ---
+var urls = [
+    "https://github.com/David-955/mobileworld/tree/v1",
+    "https://github.com/David-955/epicerie-symfony/tree/David",
+    "https://github.com/David-955/clickfast/tree/david",
+    "https://github.com/David-955/portfolio-2025",
+    "https://github.com/David-955/ParadisHardware",
+    "https://github.com/Ngo-David/Pacman",
+    "https://github.com/Ngo-David/Authentic-Go-Game",
+    "https://github.com/Ngo-David/Systeme-de-Gestion-de-Citoyens-2020",
+    "https://github.com/David-955/IceWalker",
+    "https://github.com/David-955/FlappyDunk",
+    "https://github.com/Ngo-David/GPI-Fenouil-la-fine-equipe"
+];
+var texts = [
+    "Mobile World (Symfony)",
+    "Hello Boutique (Symfony)",
+    "ClickFast (HTML, CSS, JS, Docker)",
+    "Mon portfolio (HTML, CSS, JavaScript)",
+    "Paradis du hardware (HTML, CSS)",
+    "PAC-MAN (Java)",
+    "Authentic-Go-Game (C#)",
+    "Systeme de Gestion de Citoyens (Java)",
+    "Ice Walker (Processing)",
+    "Flappy Dunk (Processing)",
+    "Fenouil (Python, Java, HTML, CSS)"
+];
+
+var livedemo = [
+    "https://mobile-world.fr/",
+    "non",
+    "https://david-955.github.io/clickfast/",
+    "https://ngo-portfolio.fr/",
+    "https://david-955.github.io/ParadisHardware/",
+    "non",
+    "non",
+    "non",
+    "https://david-955.github.io/IceWalker/",
+    "https://david-955.github.io/FlappyDunk/",
+    "non"
+];
+
+var nbr = urls.length;
+var position = 0;
+var container = document.getElementById("container-carrousel");
+var leftbutton1 = document.querySelector(".fa-circle-arrow-left");
+var rightbutton1 = document.querySelector(".fa-circle-arrow-right");
+var leftbutton2 = document.querySelector(".fa-arrow-left");
+var rightbutton2 = document.querySelector(".fa-arrow-right");
+var leftbutton3 = document.querySelector(".fa-left-long");
+var rightbutton3 = document.querySelector(".fa-right-long");
+var leftbutton4 = document.querySelector(".fa-caret-left");
+var rightbutton4 = document.querySelector(".fa-caret-right");
+
+container.style.width = (800 * nbr) + "px";
+
+// Génération dynamique des éléments du carrousel
+for (var i = 1; i <= nbr; i++) {
+    var link = document.createElement("a");
+    var description = document.createElement("p");
+    link.href = urls[i - 1];
+    link.target = "_blank";
+    description.innerHTML = texts[i - 1];
+    description.style.cssText = "background-color: #0000006d;"; 
+    description.className = "descrphoto";
+    var div = document.createElement("div");
+    div.className = "photo";
+    div.style.backgroundImage = "url('../img/im" + i + ".webp')";
+    link.appendChild(div);
+    container.appendChild(link);
+    div.appendChild(description);
+}
+
+function updateGitProjectLink() {
+    var gitLink = document.getElementById("git-projects");
+    var idx = Math.abs(position);
+    gitLink.href = urls[idx];
+    gitLink.innerHTML = '<i class="fa-brands fa-square-github"></i> <span class="github-link">GitHub</span>';
+    if (livedemo[idx] && livedemo[idx] !== "non") {
+    gitLink.innerHTML += ' &nbsp; <a href="' + livedemo[idx] + '" target="_blank" class="livedemo-link"><i class="fa-solid fa-arrow-up-right-from-square"></i> Tester</a>';
+    }
+}
+updateGitProjectLink();
+
+// Flèches version PC
+rightbutton1.onclick = function() {
+    if (position > -nbr + 1) {
+        position--;
+        container.style.transform = "translate(" + position * 800 + "px)";
+        updateGitProjectLink();
+    }
+};
+leftbutton1.onclick = function() {
+    if (position < 0) {
+        position++;
+        container.style.transform = "translate(" + position * 800 + "px)";
+        updateGitProjectLink();
+    }
+};
+// Flèches version PC 2
+rightbutton2.onclick = function() {
+    if (position > -nbr + 1) {
+        position--;
+        container.style.transform = "translate(" + position * 500 + "px)";
+        updateGitProjectLink();
+    }
+};
+leftbutton2.onclick = function() {
+    if (position < 0) {
+        position++;
+        container.style.transform = "translate(" + position * 500 + "px)";
+        updateGitProjectLink();
+    }
+};
+// Flèches version tablette
+rightbutton3.onclick = function() {
+    if (position > -nbr + 1) {
+        position--;
+        container.style.transform = "translate(" + position * 300 + "px)";
+        updateGitProjectLink();
+    }
+};
+leftbutton3.onclick = function() {
+    if (position < 0) {
+        position++;
+        container.style.transform = "translate(" + position * 300 + "px)";
+        updateGitProjectLink();
+    }
+};
+// Flèches version mobile
+rightbutton4.onclick = function() {
+    if (position > -nbr + 1) {
+        position--;
+        container.style.transform = "translate(" + position * 250 + "px)";
+        updateGitProjectLink();
+    }
+};
+leftbutton4.onclick = function() {
+    if (position < 0) {
+        position++;
+        container.style.transform = "translate(" + position * 250 + "px)";
+        updateGitProjectLink();
+    }
+};
+// --- Carrousel dynamique : fin ---
+
+
 // Initialisation de EmailJS avec ma clé publique (le code suivant est trouvable sur le site de EmailJS)
 (function(){
     emailjs.init({
@@ -57,147 +204,83 @@ window.onscroll = function() {
     }
 };
 
-// Carrousel
-
-// Tableau contenant les URL des projets associés au carrousel. Ces URL seront insérées dans les balises <a> pour chaque élément.
-var urls = [
-    "https://github.com/David-955/mobileworld/tree/v1",
-    "https://github.com/David-955/epicerie-symfony/tree/David",
-    "https://github.com/David-955/clickfast/tree/david",
-    "https://github.com/David-955/portfolio-2025",
-    "https://github.com/David-955/ParadisHardware",
-    "https://github.com/Ngo-David/Pacman",
-    "https://github.com/Ngo-David/Authentic-Go-Game",
-    "https://github.com/Ngo-David/Systeme-de-Gestion-de-Citoyens-2020",
-    "https://github.com/Ngo-David/Ice-Walker",
-    "https://github.com/Ngo-David/Flappy-Dunk",
-    "https://github.com/Ngo-David/GPI-Fenouil-la-fine-equipe"
-];
-
-// Tableau des descriptions textuelles pour chaque projet. Ces textes seront affichés sur chaque image.
-var texts = [
-    "Mobile World (Symfony)",
-    "Hello Boutique (Symfony)",
-    "ClickFast (HTML, CSS, JS, Docker)",
-    "Mon portfolio (HTML, CSS, JavaScript)",
-    "Paradis du hardware (HTML, CSS)",
-    "PAC-MAN (Java)",
-    "Authentic-Go-Game (C#)",
-    "Systeme de Gestion de Citoyens (Java)",
-    "Ice Walker (Processing)",
-    "Flappy Dunk (Processing)",
-    "Fenouil (Python, Java, HTML, CSS)"
-];
-
-nbr = 11; // Nombre de projets donc d'images
-position = 0; // Position de départ. Position sert à gérer la translation horizontale pour simuler le déplacement.
-// Container cible l’élément HTML où le carrousel est rendu.
-container = document.getElementById("container-carrousel");
-
-// Selon la version, le design de la flèche change et chaque flèche est associée à une fonction qui va redimensionner l'image dans le conteneur. Conteneur qui change de taille selon le "media screen" et c'est dans le fichier style.css que ces modifications ont lieu.
-// Boutons flèches gauche et droite de la version PC, on sélectionne les flèches via des classes CSS spécifiques.
-leftbutton1 = document.querySelector(".fa-circle-arrow-left");
-rightbutton1 = document.querySelector(".fa-circle-arrow-right");
-// Flèches gauche et droite de la version PC 2
-leftbutton2 = document.querySelector(".fa-arrow-left");
-rightbutton2 = document.querySelector(".fa-arrow-right");
-// Flèches gauche et droite de la version tablette
-leftbutton3 = document.querySelector(".fa-left-long");
-rightbutton3 = document.querySelector(".fa-right-long");
-// Flèches gauche et droite de la version mobile
-leftbutton4 = document.querySelector(".fa-caret-left");
-rightbutton4 = document.querySelector(".fa-caret-right");
-
-// Définit dynamiquement la largeur totale du conteneur en fonction du nombre d’images (nbr).
-container.style.width=(800*nbr)+"px"; /* 800 étant la largeur fixée pour chaque élément (version PC) */
-
-// Génération dynamique des éléments du carrousel
-// La boucle génère dynamiquement les éléments du carrousel : chaque projet est représenté par une balise <a> contenant une image (div avec backgroundImage) et une description (<p>).
-for (i=1; i <= nbr; i++) {
-    var link = document.createElement("a");
-    var description = document.createElement("p");
-    link.href = urls[i - 1]; // Associe un lien unique à chaque image
-    link.target = "_blank"; // ouvre le lien dans un nouvel onglet
-    description.innerHTML=texts[i - 1]; // Associe un texte unique à chaque image
-    description.className="descrphoto"; // Pour modifier l'apparence dans le fichier style.css
-    div = document.createElement("div");
-    div.className="photo";
-    // Les images se situent dans le dossier "img" et se nomment im1.webp, im2.webp, etc... (le numéro s'incrémente à chaque tour)
-    div.style.backgroundImage="url('../img/im"+i+".webp')";
-
-    // objectif <a><div><p> ... </p></div></a>
-    // Ajouter <div> dans <a>
-    link.appendChild(div);
-    // Ajouter <a> dans le container
-    container.appendChild(link);
-    // Même principe
-    div.appendChild(description);
-}
 
 // Action au clique du bouton flèche droite
+
 rightbutton1.onclick = function() {
-    // Défiler à droite c'est défiler vers -1, -2, etc... jusqu'au dernier projet
-    // S'il y a 6 projets alors on part du projet 0 jusqu'à -5, une fois "position" à -5 on s'arrête car on est à la fin du carrousel 
-    if (position > -nbr+1) {
-        position--; // Diminue la position pour faire défiler les éléments vers la gauche.
-        container.style.transform="translate("+position*800+"px)"; // container.style.transform applique une translation CSS pour déplacer visuellement le carrousel.
+    if (position > -nbr + 1) {
+        position--;
+        container.style.transform = "translate(" + position * 800 + "px)";
+        updateGitProjectLink();
     }
-}
+};
 
 // Action au clique du bouton flèche gauche
+
 leftbutton1.onclick = function() {
-    // chaque clique fait qu'on se rapproche du projet 0, la position de départ donc le premier projet
-    // La condition if (position < 0) empêche de défiler au-delà de la position de départ (projet 0).
     if (position < 0) {
-        position++; // Augmente la position pour déplacer les éléments vers la droite.
-        container.style.transform="translate("+position*800+"px)";
+        position++;
+        container.style.transform = "translate(" + position * 800 + "px)";
+        updateGitProjectLink();
     }
-}
+};
 
 // Version PC 2, largeur du conteneur et de l'image passent à 500px
+
 rightbutton2.onclick = function() {
-    if (position>-nbr+1) {
+    if (position > -nbr + 1) {
         position--;
-        container.style.transform="translate("+position*500+"px)";
+        container.style.transform = "translate(" + position * 500 + "px)";
+        updateGitProjectLink();
     }
-}
+};
+
 
 leftbutton2.onclick = function() {
     if (position < 0) {
         position++;
-        container.style.transform="translate("+position*500+"px)";
+        container.style.transform = "translate(" + position * 500 + "px)";
+        updateGitProjectLink();
     }
-}
+};
 
 // Version tablette, largeur du conteneur et de l'image passent à 300px
+
 rightbutton3.onclick = function() {
-    if (position>-nbr+1) {
+    if (position > -nbr + 1) {
         position--;
-        container.style.transform="translate("+position*300+"px)";
+        container.style.transform = "translate(" + position * 300 + "px)";
+        updateGitProjectLink();
     }
-}
+};
+
 
 leftbutton3.onclick = function() {
     if (position < 0) {
         position++;
-        container.style.transform="translate("+position*300+"px)";
+        container.style.transform = "translate(" + position * 300 + "px)";
+        updateGitProjectLink();
     }
-}
+};
 
 // Version mobile, largeur du conteneur et de l'image passent à 250px
+
 rightbutton4.onclick = function() {
-    if (position>-nbr+1) {
+    if (position > -nbr + 1) {
         position--;
-        container.style.transform="translate("+position*250+"px)";
+        container.style.transform = "translate(" + position * 250 + "px)";
+        updateGitProjectLink();
     }
-}
+};
+
 
 leftbutton4.onclick = function() {
     if (position < 0) {
         position++;
-        container.style.transform="translate("+position*250+"px)";
+        container.style.transform = "translate(" + position * 250 + "px)";
+        updateGitProjectLink();
     }
-}
+};
 
 // Formulaire
 
